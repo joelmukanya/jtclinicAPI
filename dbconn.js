8afb76637ccde29af7ac5ea70d92a7e06a352605
joelmukanya <joel@lifechoices.co.za>
2022-07-22 23:52:41 +0200

    HTTP Methods

--- dbconn.js
+++ dbconn.js
@@ -5,7 +5,8 @@ const connection = mysql.createConnection({
     'bec6ajxoan6reiui02ca-mysql.services.clever-cloud.com', 
     user: 'u0b12xpumebzixyw',
     password: 'v2oWtDWX28KLkRCKvlvN',
-    database: 'bec6ajxoan6reiui02ca'
+    database: 'bec6ajxoan6reiui02ca',
+    multipleStatements: true
 });
 // Connect
 connection.connect((err)=> {
--- index.js
+++ index.js
@@ -2,18 +2,21 @@
 const express = require('express');
 const path = require('path');
 const db = require('./dbconn');
+// const bodyparser = require('body-parse');
+//
+// app.use(bodyparser.json());
 // Create an express app
 const app = express();
 // Create a router
 const router = express.Router();
 // Port
-const port = 4000;
+const port = process.env.port || 4000;
 // Fetch data
-router.get('^/$|/index.html', (req, res)=> {
+router.get('^/$|/clinic', (req, res)=> {
     // Query
     const strQry = 
     `
-    SELECT CONCAT(d.firstname, ' ', d.lastname) 'Doctor Fullname', d.dentistAge, 
+    SELECT d.id, CONCAT(d.firstname, ' ', d.lastname) 'Doctor Fullname', d.dentistAge, 
     d.contactNumb, d.practiceNumb, CONCAT(p.firstname, ' ', p.lastname) 'Patient Fullname', 
     p.paymentMethods, p.medicalAid
     FROM dentists d
@@ -25,6 +28,52 @@ router.get('^/$|/index.html', (req, res)=> {
         res.send(data);
     })
 });
+
+// Fetch one record
+router.get('/clinic/:id', (req, res)=> {
+    // Query
+    const strQry = 
+    `
+    SELECT d.id, CONCAT(d.firstname, ' ', d.lastname) 'Doctor Fullname', d.dentistAge, 
+    d.contactNumb, d.practiceNumb
+    FROM dentists d
+    WHERE d.id = ?;
+    `;
+    db.query(strQry,[req.params.id], (err, data)=> {
+        if(err) throw err;
+        res.send(data);
+    })
+});
+
+//Post
+// Add a new record
+router.post('/clinic', (req, res)=> {
+    // Query
+    const strQry = 
+    `
+    INSERT INTO dentists
+    VALUES(?, ?, ?);
+    `;
+    db.query(strQry,[req.params.id], (err, data)=> {
+        if(err) throw err;
+        res.send(data);
+    })
+});
+
+//Delete a record
+router.delete('/clinic', (req, res)=> {
+    // Query
+    const strQry = 
+    `
+    DELETE FROM dentists 
+    WHERE id = ?;
+    `;
+    db.query(strQry,[req.params.id], (err, data, fields)=> {
+        if(err) throw err;
+        res.send('A row was affected');
+    })
+});
+
 
 // express.json(): It a middleware
 app.use(router, express.json(), express.urlencoded({
